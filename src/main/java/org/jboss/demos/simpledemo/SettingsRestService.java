5071c026f91171f3b93a05813e69c076de4eb352
tqvarnst <tqvarnst@redhat.com>
2015-08-28 17:15:47 +0200

    Fix for bug 1235

--- src/main/java/org/jboss/demos/simpledemo/SettingsRestService.java
+++ src/main/java/org/jboss/demos/simpledemo/SettingsRestService.java
@@ -12,47 +12,52 @@ import javax.ws.rs.Produces;
 
 @Path("/")
 public class SettingsRestService {
-	
-	private Map<String,String> settingsMap = new HashMap<String,String>(); 
-	
-	public SettingsRestService() {
-		String hostname = this.getHostname();
-		settingsMap.put("hostname", hostname);
-		settingsMap.put("os", String.format("%1$s-%3$s (%2$s)", System.getProperty("os.name"),System.getProperty("os.arch"),System.getProperty("os.version")));
-	}
-	
-	@GET
-    @Path("settings")
-    @Produces({ "application/json" })
-    public Map<String,String> getSettings() throws IOException {
-        return settingsMap;
-    }
 
-	/**
-	 * This demo app is designed to run in a docker container, hence we could assume linux, but to be able to test the code, we added support for windows and Mac OS X also. 
-	 * @return
-	 * @throws IOException 
-	 */
-	private String getHostname() {
+	private Map<String, String> settingsMap = new HashMap<String, String>();
+
+	public SettingsRestService() throws InterruptedException, IOException {
+		settingsMap.put("hostname", getHostname());
+		settingsMap.put("os", getOperatingSystemInfo());
+	}
+
+	@GET
+	@Path("settings")
+	@Produces({ "application/json" })
+	public Map<String, String> getSettings() throws IOException {
+		return settingsMap;
+	}
+
+	private String getHostname() throws InterruptedException, IOException {
 		String OS = System.getProperty("os.name");
 		if (OS.indexOf("win") >= 0) {
-            return System.getenv("COMPUTERNAME");
-        } else if (OS.indexOf("nix") >= 0 || OS.indexOf("nux") >= 0 || OS.indexOf("Mac OS")>=0 ) {
-        	try {
-        		if(Runtime.getRuntime().exec("sh -f /.dockerinit").waitFor()==0) {
-					return "Running in a docker container, where hostname command isn't available";
-				} else {
-					BufferedReader reader = new BufferedReader(new InputStreamReader(Runtime.getRuntime().exec("hostname").getInputStream()));
-					return reader.readLine();
-				}
-			} catch (IOException e) {
-				return "Failed to get from native hostname command";
-			} catch (InterruptedException e) {
-				return "Failed to get from native hostname command";
+			return System.getenv("COMPUTERNAME");
+		} else if (OS.indexOf("nix") >= 0 || OS.indexOf("nux") >= 0
+				|| OS.indexOf("Mac OS") >= 0) {
+			if (Runtime.getRuntime().exec("sh -f /.dockerinit").waitFor() == 0) {
+				return "Running in a docker container, where hostname command isn't available";
+			} else {
+				BufferedReader reader = new BufferedReader(
+						new InputStreamReader(Runtime.getRuntime()
+								.exec("hostname").getInputStream()));
+				return reader.readLine();
 			}
-        }
-        else {
-        	return "Unknown";
-        }
+		} else {
+			return "Unknown";
+		}
+	}
+
+	private String getOperatingSystemInfo() throws InterruptedException,
+			IOException {
+		if (Runtime.getRuntime().exec("sh -f /etc/redhat-release").waitFor() == 0) {
+			BufferedReader reader = new BufferedReader(new InputStreamReader(
+					Runtime.getRuntime().exec("cat /etc/redhat-release")
+							.getInputStream()));
+			return reader.readLine();
+		} else {
+			return String.format("%1$s-%3$s (%2$s)",
+					System.getProperty("os.name"),
+					System.getProperty("os.arch"),
+					System.getProperty("os.version"));
+		}
 	}
 }
