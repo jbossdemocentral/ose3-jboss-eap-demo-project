365a14893d62909bc34101fc5f3ad302d6c57b89
tqvarnst <tqvarnst@redhat.com>
2015-09-03 23:33:03 +0200

    Version 1

--- src/main/resources/oc/oc-setup.sh
+++ src/main/resources/oc/oc-setup.sh
@@ -5,8 +5,8 @@ if [ "$#" -ne 1 ]; then
   exit 1
 fi
 
-oc new-project $1
+oc new-project $1 --display-name="JBoss EAP Demo Project" --description="This is a simple JBoss EAP demo project"
 oc create -f scripts/jboss-eap-imagestream.json
 oc create -f scripts/simple-eap-temlpate.json
-#oc new-app --template=jboss-demo-example
-#oc start-build jbossdemo
+oc new-app --template=jboss-demo-example
+oc start-build jbossdemo
